480fef300a9d38812db510f29262632f769bb1c2
dev <devsharma706733@gmail.com>
2025-02-18 03:08:18 +0530

    add cookie-parser and cors middleware, implement error handling, and create utility classes for API responses and errors

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import dotenv from "dotenv";
 // import { DB_NAME } from "./constants.js";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./.env"
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+    app.on("error",(error)=>{
+        console.log("Server error: ", error);
+        throw error;
+    })
+})
+.catch((error)=>{
+    console.log("MONGODB connection failed: ", error);
+})
 
 /*
 import express from "express";
