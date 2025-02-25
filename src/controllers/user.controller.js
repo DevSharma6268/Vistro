b841126600560245ade091151d79bd5818b7c58b
dev <devsharma706733@gmail.com>
2025-02-25 18:25:31 +0530

    add user channel profile and watch history retrieval functionalities

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -378,6 +378,124 @@ const updateUserCoverImage = asyncHandler(async (req,res)=>{
     .json(new ApiResponse(200,updatedUser,"Cover image updated successfully"))
 })
 
+const getUserChannelProfile = asyncHandler(async (req,res)=>{
+    const {username} = req.params
+
+    if(username?.trim()){
+        throw new ApiError(400,"username is required")
+    }
+
+    const channel = await User.aggregate(
+        [
+            {
+                $match:{
+                    username:username?.toLowercase()
+                }
+            },
+            {
+                $lookup:{
+                    from:"subscriptions",
+                    localField:"_id",
+                    foreignField:"channel",
+                    as:"subscribers"
+                }
+            },
+            {
+                $lookup:{
+                    from:"subscriptions",
+                    localField:"_id",
+                    foreignField:"subscriber",
+                    as:"subscribedTo"
+                }
+            },
+            {
+                $addFields:{
+                    subscriberCount:{$size:"$subscribers"},
+                    channelSubscribedToCount:{$size:"$subscribedTo"},
+                    isSubscribed:{
+                        $cond:{
+                            if:{
+                                $in:[req.user?._id,"$subscribers.subscriber"]
+                            },
+                            then:true,
+                            else:false
+                        }
+                    }
+                },
+                
+            },
+            {
+                $project:{
+                    fullName:1,
+                    username:1,
+                    avatar:1,
+                    coverImage:1,
+                    subscriberCount:1,
+                    channelSubscribedToCount:1,
+                    isSubscribed:1
+                }
+            }
+        ]
+    )
+
+    if(!channel?.length){
+        throw new ApiError(404,"Channel not found")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,channel[0],"Channel fetched successfully"))
+
+})
+
+const getWatchHistory = asyncHandler (async (req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{$first:"$owner"}
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,user[0].watchHistory,"Watch history fetched successfully"))
+})
+
 export {registerUser
     ,loginUser
     ,logoutUser
@@ -386,5 +504,7 @@ export {registerUser
     ,getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
 }
